e853953736cf4ccbdc0c353aa819088abf38e44b
Mr. Bartucz <jobartucz@isd535.org>
2021-12-09 13:22:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const REPLIT = false
 const MAX_STARS = 25
 const STAR = "★"
 const FORMAT = "%PLACE) %SCORE %STARS %NAME (%SCHOOL)"
-const PATHTOINDIVIDUAL = "/var/www/html/python/AOCbot/data_file.json"
-const PATHTOTEAMS = "/var/www/html/python/AOCbot/team_file.json"
-const PATHTOCSV = "/var/www/html/python/AOCbot/users.json"
+const PATHTOINDIVIDUAL = "/python/AOCbot/data_file.json"
+const PATHTOTEAMS = "/python/AOCbot/team_file.json"
+const PATHTOCSV = "/python/AOCbot/users.json"
 
 var schools = {}
 var schoolsParticipation = {}
